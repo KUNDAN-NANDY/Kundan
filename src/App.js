5e5291100d00b96777ff285c806d3bc283d82747
KUNDAN-NANDY <kundannandy27598@gmail.com>
2021-02-05 21:09:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react"
+import Home from "./page/home"
+function App() {
+  return (
+    <div>
+    <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Home () {
+  return(
+    <div>
+    <h1>hello react</h1>
+    <p>Kundan</p>
+    </div>
+    );
+}
+export default Home;
